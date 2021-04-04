891a5a9942706a144cf695215f250ec57a0b7b8d
Stefano Bielli <stefano.bielli@gmail.com>
2021-04-05 01:05:06 +0200

    auth token controller

--- src/main/java/com/inkpot/api/controller/auth/AuthController.java
+++ src/main/java/com/inkpot/api/controller/auth/AuthController.java
@@ -1,25 +1,28 @@
 package com.inkpot.api.controller.auth;
 
-import io.quarkus.security.identity.CurrentIdentityAssociation;
+import com.inkpot.api.iam.Authenticator;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("auth")
 @ApplicationScoped
 public class AuthController {
 
-    @Inject
-    private CurrentIdentityAssociation association;
+    private final Authenticator authenticator;
 
-    @POST
+    @Inject
+    public AuthController(Authenticator authenticator) {
+        this.authenticator = authenticator;
+    }
+
+    @GET
     @Path("token")
     public String token() {
-        var identity = association.getIdentity();
-
-        return identity.getPrincipal().getName();
+        return authenticator.currentAuthenticatedUser()
+                .generateToken().asString();
     }
 
 }
